c421b8b922253983fdf1012b9b7399be785fbaac
Konstantin Voykov <koykov0@gmail.com>
2023-02-28 22:28:26 +0200

    Extend entgen and HTML object.

--- cmd/entgen/mod_html.go
+++ cmd/entgen/mod_html.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"sort"
+	"strings"
 
 	"github.com/koykov/entry"
 )
@@ -60,16 +61,24 @@ func (m htmlModule) Compile(w moduleWriter, input, target string) (err error) {
 
 	_, _ = w.WriteString("var (\n")
 
+	// Write list of HTML objects.
 	_, _ = w.WriteString("__bufH = []HTML{\n")
 	for i := 0; i < len(names); i++ {
-		var e entry.Entry32
+		var en, ev entry.Entry32
 		lo := len(buf)
-		buf = append(buf, raw[names[i]].Characters...)
+		buf = append(buf, names[i]...)
 		hi := len(buf)
-		e.Encode(uint16(lo), uint16(hi))
+		en.Encode(uint16(lo), uint16(hi))
+
+		lo = len(buf)
+		buf = append(buf, raw[names[i]].Characters...)
+		hi = len(buf)
+		ev.Encode(uint16(lo), uint16(hi))
 
 		_, _ = w.WriteString("HTML{name:")
-		_, _ = w.WriteString(fmt.Sprintf("0x%08x", e))
+		_, _ = w.WriteString(fmt.Sprintf("0x%08x", en))
+		_, _ = w.WriteString(",val:")
+		_, _ = w.WriteString(fmt.Sprintf("0x%08x", ev))
 		_, _ = w.WriteString(",cp:")
 		var cp int64
 		cp = int64(raw[names[i]].Codepoints[0]) << 32
@@ -81,10 +90,51 @@ func (m htmlModule) Compile(w moduleWriter, input, target string) (err error) {
 	}
 	_, _ = w.WriteString("}\n")
 
+	// Write name/HTML registry.
 	_, _ = w.WriteString("__bufHN = map[string]int{\n")
 	for i := 0; i < len(names); i++ {
 		_, _ = w.WriteString(`"`)
 		_, _ = w.WriteString(names[i])
+		_, _ = w.WriteString(`":`)
+		_, _ = w.WriteString(fmt.Sprintf("0x%04x", i))
+		_, _ = w.WriteString(",\n")
+	}
+	_, _ = w.WriteString("}\n")
+
+	// Write codepoint/HTML registry.
+	var hcp = map[int64]struct{}{}
+	_, _ = w.WriteString("__bufHCP = map[int64]int{\n")
+	for i := 0; i < len(names); i++ {
+		var cp int64
+		cp = int64(raw[names[i]].Codepoints[0]) << 32
+		if len(raw[names[i]].Codepoints) > 1 {
+			cp = cp | int64(raw[names[i]].Codepoints[1])
+		}
+		if _, ok := hcp[cp]; ok {
+			continue
+		}
+		hcp[cp] = struct{}{}
+		_, _ = w.WriteString(fmt.Sprintf("0x%08x", cp))
+		_, _ = w.WriteString(":")
+		_, _ = w.WriteString(fmt.Sprintf("0x%04x", i))
+		_, _ = w.WriteString(",\n")
+	}
+	_, _ = w.WriteString("}\n")
+
+	// Write char/HTML registry.
+	var hc = map[string]struct{}{}
+	_, _ = w.WriteString("__bufHC = map[string]int{\n")
+	for i := 0; i < len(names); i++ {
+		c := raw[names[i]].Characters
+		c = strings.ReplaceAll(c, "\\", "\\\\")
+		c = strings.ReplaceAll(c, "\n", "\\n")
+		c = strings.ReplaceAll(c, "\"", `\"`)
+		if _, ok := hc[c]; ok {
+			continue
+		}
+		hc[c] = struct{}{}
+		_, _ = w.WriteString(`"`)
+		_, _ = w.WriteString(c)
 		_, _ = w.WriteString(`":`)
 		_, _ = w.WriteString(fmt.Sprintf("0x%04x", i))
 		_, _ = w.WriteString(",\n")
--- html.go
+++ html.go
@@ -4,5 +4,6 @@ import "github.com/koykov/entry"
 
 type HTML struct {
 	name entry.Entry32
+	val  entry.Entry32
 	cp   int64
 }
