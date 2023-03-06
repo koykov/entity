36841e58fc4782ac2cc20ee452dab7c835fcc8bd
Konstantin Voykov <koykov0@gmail.com>
2023-03-06 15:39:04 +0200

    html: enable rest of testcases.

--- html/unescape_test.go
+++ html/unescape_test.go
@@ -10,10 +10,10 @@ var stagesUnesc = []stage{
 	{"decimalEntity", "Delta = &#916; ", "Delta = Δ "},
 	{"hexadecimalEntity", "Lambda = &#x3bb; = &#X3Bb ", "Lambda = λ = λ "},
 	{"numericEnds", "&# &#x &#128;43 &copy = &#169f = &#xa9", "&# &#x €43 © = ©f = ©"},
-	// {"numericReplacements", "Footnote&#x87;", "Footnote‡"},
-	// {"copySingleAmpersand", "&", "&"},
-	// {"copyAmpersandNonEntity", "text &test", "text &test"},
-	// {"copyAmpersandHash", "text &#", "text &#"},
+	{"numericReplacements", "Footnote&#x87;", "Footnote‡"},
+	{"copySingleAmpersand", "&", "&"},
+	{"copyAmpersandNonEntity", "text &test", "text &test"},
+	{"copyAmpersandHash", "text &#", "text &#"},
 }
 
 func TestUnescape(t *testing.T) {
