f44f0f2568b480faa13cd36dce7511c9c6222c51
Konstantin Voykov <koykov0@gmail.com>
2023-03-12 15:55:07 +0200

    html: cover rune unescaper with test/bench.

--- html/common_test.go
+++ html/common_test.go
@@ -5,3 +5,15 @@ type stage struct {
 	raw    string
 	expect string
 }
+
+func assertR(a, b []rune) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ html/unescape_rune_test.go
@@ -0,0 +1,32 @@
+package html
+
+import (
+	"testing"
+
+	"github.com/koykov/fastconv"
+)
+
+func TestUnescapeRune(t *testing.T) {
+	for _, stage := range stagesUnesc {
+		t.Run(stage.key, func(t *testing.T) {
+			r := UnescapeRune(stage.raw)
+			e := fastconv.AppendS2R(nil, stage.expect)
+			if !assertR(r, e) {
+				t.FailNow()
+			}
+		})
+	}
+}
+
+func BenchmarkUnescapeRune(b *testing.B) {
+	for _, stage := range stagesUnesc {
+		b.Run(stage.key, func(b *testing.B) {
+			b.ReportAllocs()
+			var buf []rune
+			for i := 0; i < b.N; i++ {
+				buf = AppendUnescapeRune(buf[:0], stage.raw)
+			}
+			_ = buf
+		})
+	}
+}
